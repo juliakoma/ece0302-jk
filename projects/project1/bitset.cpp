25052709dbd98a910fb3e45d7e8033f4c235d46e
juliakoma <juk97@pitt.edu>
2023-05-22 11:14:44 -0400

    done

--- projects/project1/bitset.cpp
+++ projects/project1/bitset.cpp
@@ -2,8 +2,8 @@
 
 Bitset::Bitset() {
     // TODO
-    //Default constructor : loop that will set all bits to 0
-    for (intmax_t i = 0; i < size; i++)
+    // Default constructor : loop that will set all bits to 0
+    for (intmax_t i = 0; i < 8; i++)
     {
         bits[i] = false;
     }
@@ -12,6 +12,8 @@ Bitset::Bitset() {
 
 Bitset::Bitset(intmax_t size) {
     // TODO
+    // Constructor with size N parameter
+    // sets all bits to 0 and will exit code if bitset is invalid
 }
 
 Bitset::Bitset(const std::string & value) {
