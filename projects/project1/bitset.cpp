d31a0616a9dc0b07b6520c0412b741a3fbb6696a
juliakoma <juk97@pitt.edu>
2023-05-22 11:14:51 -0400

    done

--- projects/project1/bitset.cpp
+++ projects/project1/bitset.cpp
@@ -3,7 +3,11 @@
 Bitset::Bitset() {
     // TODO
     // Default constructor : loop that will set all bits to 0
-    for (intmax_t i = 0; i < 8; i++)
+    N = 8;
+    valid = true;
+
+    bits = new bool[8];
+    for (intmax_t i = 0; i < N; i++)
     {
         bits[i] = false;
     }
@@ -13,15 +17,125 @@ Bitset::Bitset() {
 Bitset::Bitset(intmax_t size) {
     // TODO
     // Constructor with size N parameter
-    // sets all bits to 0 and will exit code if bitset is invalid
+    // Error check if bitset is valid, will exit code if invalid
+    if (size <= 0)
+    {
+        valid = false;
+        return;
+    }
+
+    //If bitset is valid, will set parameter size to N and create bits array of size N
+    N = size;
+    bits = new bool[N];
+    valid = true;
+
+    for (intmax_t i = 0; i < N; i++)
+    {
+        bits[i] = false;
+    }
 }
 
 Bitset::Bitset(const std::string & value) {
-    // TODO
+    // TODO        
+    valid = true;
+    N = value.length();
+    
+    // initalizing valid bitset and error checking for only 0 or 1
+    for (intmax_t i = 0; i < N; i++)
+    {
+        if (value[i] != '0' && value[i] != '1'){
+            valid = false;
+            return;
+        }
+    }
+
+    // If bitset is valid, then will iterate through string parameter and fill the bits array
+    if (valid){
+        N = value.length();
+        bits = new bool[N];
+        for (intmax_t i = 0; i < N; i++)
+        {
+            if(value[i] == '0'){
+                bits[i] = 0;
+            }
+            else{
+                bits[i] = 1;
+            }
+        }
+    }
 }
 
 Bitset::~Bitset() {
     // TODO
+    //Destructor
+    delete [] bits;
 }
 
 // TODO: other methods
+
+//Function to return the bit size
+intmax_t Bitset::size() const{
+    return N;
+}
+
+//method to determine if bitset is valid
+bool Bitset::good() const{
+    return valid;
+}
+
+//Method to set nth bit to 1
+void Bitset::set(intmax_t index){
+    //if index is not between 0 and N-1 then code will exit
+    if (index < 0 || index >= N){
+        valid = false;
+        return;
+    }
+    //if index is in the range, nth value is set to 1
+    bits[index] = true;
+
+}
+
+//Method to reset the nth bit to 0
+void Bitset::reset(intmax_t index){
+    if (index < 0 || index >= N)
+    {
+        valid = false;
+        return;
+    }
+    else{
+        bits[index] = false;
+    }
+}
+
+//Method to toggle the nth bit (1 to 0 or 0 to 1)
+void Bitset::toggle(intmax_t index){
+    if (index < 0 || index > N){
+        valid = false;
+        return;
+    }
+    else
+    {
+        bits[index] = !bits[index];
+    }
+}
+
+//Method to check if nth bit is set
+bool Bitset::test(intmax_t index){
+    if (index < 0 || index >= N){
+        valid = false; 
+        return false; 
+    }
+   return bits[index] == true;
+}
+
+//Method to return bitset as a string of characters
+std::string Bitset::asString() const{
+    std::string result;
+
+    for (intmax_t i = 0; i < N; i++)
+    {        
+        result += bits[i] ? '1' : '0';
+    }
+
+    return result;
+}
--- projects/project1/bitset.hpp
+++ projects/project1/bitset.hpp
@@ -7,47 +7,52 @@
 class Bitset{
 public:
 
-  // TODO COMMENT
+  // Default Constructor
   Bitset();
 
-  // TODO COMMENT
+  // Constructor with parameter size, will set N equal to parameter size and then create bit array with N
   Bitset(intmax_t size);
 
-  // TODO COMMENT
+  // Constructor with parameter string
+  // Function includes an error check for a valid string and then size of the bitset will be allocated according to the length of the string 
   Bitset(const std::string & value);
 
-  // TODO COMMENT
+  // Destructor
   ~Bitset();
 
   Bitset(const Bitset & ) = delete;
   Bitset & operator=(const Bitset &) = delete;
 
-  // TODO COMMENT
+  // Getter function that returns size
   intmax_t size() const;
 
-  // TODO COMMENT
+  // Method to return if bitset is valid
   bool good() const;
 
-  // TODO COMMENT
+  // Method to set nth bit to 1
   void set(intmax_t index);
 
-  // TODO COMMENT
+  // Method that will reset the nth bit to 0
   void reset(intmax_t index);
 
-  // TODO COMMENT
+  // Method to toggle the nth bit (1 to 0 or 0 to 1)
   void toggle(intmax_t index);
 
-  // TODO COMMENT
+  // Method to check if the nth bit is set
   bool test(intmax_t index);
 
-  // TODO COMMENT
+  // Method to return bitset as a string of characters
   std::string asString() const;
 
 private:
-    intmax_t size;
-    bool* bits;
+    //Creating intmax N value that will hold size or length of array
+    intmax_t N;
+
+    // Initializing pointer to a bool array bits
+    bool* bits = new bool[8];
+
+    // Bool value of valid 
     bool valid;
-  // TODO
 };
 
 #endif
